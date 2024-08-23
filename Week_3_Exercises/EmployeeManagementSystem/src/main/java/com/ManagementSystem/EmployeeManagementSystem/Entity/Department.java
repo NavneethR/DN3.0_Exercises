1d105be5b29f4d821bc413c0f93e212c8613036c
NavneethR <navneethramesh74@gmail.com>
2024-08-23 17:33:18 +0530

    Added some Week 3 exercises

---
+++ Week_3_Exercises/EmployeeManagementSystem/src/main/java/com/ManagementSystem/EmployeeManagementSystem/Entity/Department.java
@@ -0,0 +1,23 @@
+package com.example.employeemanagementsystem.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Employee> employees;
+}
